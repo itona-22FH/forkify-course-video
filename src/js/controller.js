9e84b1e4dceaedf508f2bdf171fb3e3b144b6c5f
itona-22FH <naov7700@gmail.com>
2022-03-18 18:59:34 +0900

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -110,6 +110,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function(){
+  console.log('Welcome to the application')
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -118,7 +122,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome')
+  newFeature();
 };
 
 
