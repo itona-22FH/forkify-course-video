293aa20705097aa0d8f356e8d9bc7594bebeef92
itona-22FH <naov7700@gmail.com>
2022-03-18 18:47:06 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = '1f62e4e9-7167-4362-bcbd-d591f27ff87e';
+export const MODAL_CLOSE_SEC = 2.5;
